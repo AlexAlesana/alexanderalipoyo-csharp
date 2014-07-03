1a1b0a0722d27982bd9b56a222ca6e42b94b609a
alex <darkdrakez_14@yahoo.com>
2014-07-03 18:18:48 +0800

    assignment

---
+++ alex-assign/add.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace alex_assign
+{
+    partial class add
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numtxt = new System.Windows.Forms.TextBox();
+            this.counter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(114, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "CLOSE";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 38);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "ADD";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numtxt
+            // 
+            this.numtxt.Location = new System.Drawing.Point(12, 12);
+            this.numtxt.Name = "numtxt";
+            this.numtxt.Size = new System.Drawing.Size(177, 20);
+            this.numtxt.TabIndex = 5;
+            this.numtxt.TextChanged += new System.EventHandler(this.numtxt_TextChanged);
+            // 
+            // counter
+            // 
+            this.counter.AutoSize = true;
+            this.counter.Location = new System.Drawing.Point(143, 43);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(13, 13);
+            this.counter.TabIndex = 8;
+            this.counter.Text = "0";
+            this.counter.Visible = false;
+            // 
+            // add
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 72);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.numtxt);
+            this.Name = "add";
+            this.Text = "add";
+            this.Load += new System.EventHandler(this.add_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox numtxt;
+        public System.Windows.Forms.Label counter;
+    }
+}
---
+++ alex-assign/add.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace alex_assign
+{
+    public partial class add : Form
+    {
+        public add()
+        {
+            InitializeComponent();
+        }
+        public add(Form1 frmhand)
+        {
+            InitializeComponent();
+            handleform1 = frmhand;
+         
+        }
+        private Form1 handleform1;
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            {
+                
+                int num = Convert.ToInt32(numtxt.Text);
+                int coun = Convert.ToInt32(counter.Text);
+                int result = coun + num;
+                counter.Text = result.ToString();
+
+                handleform1.WriteTobutton(counter.Text);
+                }
+        }
+       
+        private void add_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void numtxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
